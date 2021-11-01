19d2cf10e5cf52501774b679d7a2b060b9dea0b4
Dabin Lee <dabin.lee@outlook.com>
2021-11-01 22:16:01 +0800

    updated deSwitch

--- contracts/DeSwitch.sol
+++ contracts/DeSwitch.sol
@@ -4,20 +4,27 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract DeSwitch {
-  event LogForGameRegistered(uint gameId, address gameOwnerAddress);
-  event LogForGameRentalRequested(uint gameId, address gameRenter, address gameOwnerAddress);
-  event LogFOrGameRentalTxnCompleted(uint gameId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRegistered(uint gameid, uint trackingId, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRentalRequested(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRentalShipped(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddresss, uint rentalRate, uint depositRequired);
+  event LogForGameRentalReceived(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRentalReturnRequested(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRentalReturnShipped(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogFOrGameRentalTxnCompleted(uint gameid, uint trackingId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  
   enum gameState {
     Available,
+    Reserved,
     Shipped,
     Rented,
-    Sold
+    NotAvailable
   }
+
   struct Game{
     uint gameid;
     uint trackingId;
     uint rentalRate;
-    uint sellRate;
+    uint deposit;
     uint timeRentalStart;
     address payable gameOwner;
     address payable gamebuyer;
@@ -30,6 +37,11 @@ contract DeSwitch {
 
   
     constructor() public {
+      trackingId = 1;
+  }
+
+  function findGamesForRentByGameID(uint gameId) public view returns (uint){
+    return games[gameId].trackingId;
   }
 
   function registerGame(uint gameId, uint rentalRate, uint depositRequired, gameState buyOption, uint postalCode) public returns(bool, uint){
@@ -51,27 +63,19 @@ contract DeSwitch {
     //Verify the state of trackingId to be "Available" using a modifier
     //return true or false if the rentGame attempt was successful
     //check if msg.value == games[trackingId].rentalRate
-    //at the end of this function the state of the Game should be set to "TransactionInProgress"
+    //at the end of this function the state of the Game should be set to "Reserved"
   }
 
-  function buyGame(uint trackingId) public payable returns(bool){
-    //should be "payable" as this requires ether to be transffered to the contract
-    //should be "public" as anyone should be able to buy the game 
-    //Verify the state of trackingId to be either "Available" using a modifier
-    //return true or false if the rentGame attempt was successful
-    //check if msg.value == games[trackingId].sellRate
-    //at the end of this function the state of the Game should be set to "TransactionInProgress"
-  }
 
   function shipGame(uint trackingId) public returns(bool){
     //To be called by the gameOwner to account for state update to "Shipped"
     //Only the gameOwner with same account address as games[trackingId].gameOnwer can execute this function
-    //Require the games[trackingId].state to be "AvailableForRent" or "AvailableForBuy"
+    //Require the games[trackingId].state to be "Available" or "Rented"
   } 
 
   function receiveGame(uint trackingId) public returns(bool){
-    //To be called by the gameBuyer or gameRenter to account for state change to "Rented" or "Sold"
-    //This function triggers payment to the gameOwner address if the transaction completed was "buy" instead of "rent"
+    //To be called by the gameRenter to account for state change to "Rented"
+    //This function triggers payment to the gameOwner address
     //This function makes the time to be loggged as the start of rental as timeRentalStart
 
   }
