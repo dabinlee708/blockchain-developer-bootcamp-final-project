d149b05eb3b2aa9ec584c174f96c8519c3f5e961
Dabin Lee <dabin.lee@outlook.com>
2021-11-02 00:04:05 +0800

    Register Game seems to work????

--- DeSwitch.js
+++ DeSwitch.js
@@ -1,33 +1,321 @@
 
-const ssAddress = '0x6c6F4B14Fe020801Ebb90a4C18581B852d7B809B'
+const ssAddress = '0x61B28a04c63961BfCdb2169967D45E94d0e4c2EB'
 const ssABI = [
-	{
-		"inputs": [],
-		"name": "retrieve",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "num",
-				"type": "uint256"
-			}
-		],
-		"name": "store",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "gameId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "registerId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "gameRenter",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "gameOwnerAddress",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "rentalRate",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "depositRequired",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogFOrGameRentalTxnCompleted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "gameId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "registerId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "gameOwnerAddress",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "rentalRate",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "depositRequired",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogForGameRegistered",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "gameId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "registerId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "gameRenter",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "gameOwnerAddress",
+          "type": "address"
+        }
+      ],
+      "name": "LogForGameRentalRequested",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "games",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "gameid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "gregisterId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "rentalRate",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "depositRequired",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "gameOwner",
+          "type": "address"
+        },
+        {
+          "internalType": "enum DeSwitch.gameState",
+          "name": "state",
+          "type": "uint8"
+        },
+        {
+          "internalType": "uint256",
+          "name": "timeRentalStart",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "gameRenter",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gameId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_rentalRate",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_depositRequired",
+          "type": "uint256"
+        }
+      ],
+      "name": "registerGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "trackingId",
+          "type": "uint256"
+        }
+      ],
+      "name": "rentGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "trackingId",
+          "type": "uint256"
+        }
+      ],
+      "name": "buyGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "trackingId",
+          "type": "uint256"
+        }
+      ],
+      "name": "shipGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "trackingId",
+          "type": "uint256"
+        }
+      ],
+      "name": "receiveGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "trackingId",
+          "type": "uint256"
+        }
+      ],
+      "name": "returnGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
 console.log("hello dapdeverlopers")
 window.addEventListener('load',function(){
     if (typeof window.ethereum !== 'undefined'){
@@ -59,15 +347,18 @@ metamaskEnable.onclick = async () => {
 
 const ssSubmit = document.getElementById("ss-input-button");
 ssSubmit.onclick = async () => {
-    const ssValue = document.getElementById("ss-number").value;
-    console.log(ssValue)
+    const gameid = document.getElementById("gameid").value;
+	const rentalRate = document.getElementById("rentalRate").value;
+	const depositRate = document.getElementById("depositRate").value;
+
+    console.log(gameid, rentalRate, depositRate)
 
     var web3 = new Web3(window.ethereum)
 
-    const simpleStorage = new web3.eth.Contract(ssABI, ssAddress)
+    const deSwitch = new web3.eth.Contract(ssABI, ssAddress)
 
-    simpleStorage.setProvider(window.ethereum)
+    deSwitch.setProvider(window.ethereum)
 
-    await simpleStorage.methods.store(ssValue).send({from: ethereum.selectedAddress})
+    await deSwitch.methods.registerGame(gameid, rentalRate, depositRate).send({from: ethereum.selectedAddress})
     
 }
--- contracts/DeSwitch.sol
+++ contracts/DeSwitch.sol
@@ -4,45 +4,67 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract DeSwitch {
-  event LogForGameRegistered(uint gameId, address gameOwnerAddress);
-  event LogForGameRentalRequested(uint gameId, address gameRenter, address gameOwnerAddress);
-  event LogFOrGameRentalTxnCompleted(uint gameId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRegistered(uint gameId, uint registerId, address gameOwnerAddress, uint rentalRate, uint depositRequired);
+  event LogForGameRentalRequested(uint gameId, uint registerId, address gameRenter, address gameOwnerAddress);
+  event LogFOrGameRentalTxnCompleted(uint gameId, uint registerId, address gameRenter, address gameOwnerAddress, uint rentalRate, uint depositRequired);
   enum gameState {
     Available,
     Shipped,
     Rented,
     Sold
   }
+
   struct Game{
     uint gameid;
-    uint trackingId;
+    uint gregisterId;
     uint rentalRate;
-    uint sellRate;
-    uint timeRentalStart;
+    uint depositRequired;
     address payable gameOwner;
-    address payable gamebuyer;
-    address payable gameRenter;
     gameState state;
+    uint timeRentalStart;
+    address payable gameRenter;
   }
 
-  uint trackingId;
-  mapping (uint => Game) public games;
+  
+  mapping (address => Game) public games;
+  uint registerId = 0 ;
 
   
     constructor() public {
   }
 
-  function registerGame(uint gameId, uint rentalRate, uint depositRequired, gameState buyOption, uint postalCode) public returns(bool, uint){
+  function registerGame(uint _gameId, uint _rentalRate, uint _depositRequired) public returns(bool, uint){
+    
+    
     //should be a public function as anyone should be able to put their game for sale/rental
     //Users register games they own (Ex. Switch Games) as "Available"
     //GameID refers to gameId from http://nswdb.com/ to avoid confusion between different game titles
     //Require checks for validity of values submitted for required arguments
     //Set gameState as "Available"
     //Set gameOwner as msg.sender
-    //Set gameBuyer and gameRenter will be empty and get updated once a buyer/owner triggers the rentGame/buyGame function
+    //Set gameBuyer and gameRenter will be empty and get updated once a buyer triggers the rentGame function
     //returns true/false if registeration was successful and a trackingId tagged to the registered game
+    
+    //Increase the registerId counter by 1 so that the next game gets registered with the next registerId.
+    //Consider hashing/randomizing instead of incremental values, can consider sha256(msg.sender+registerID)
+    registerId += 1;
+
+    //Create a Game struct with the provided values
+    games[msg.sender] = Game({
+      gameid : _gameId,
+      gregisterId : registerId,
+      rentalRate : _rentalRate,
+      depositRequired : _depositRequired,
+      gameOwner : msg.sender,
+      state : gameState.Available,
+      timeRentalStart : 0,
+      gameRenter : address(0)
+    });
+  
     //Emits appropriate event to log the chagnes 
-    //Create a random trackingId 
+    emit LogForGameRegistered(_gameId, registerId, msg.sender, _rentalRate, _depositRequired);
+
+    return (true, registerId);
   }
 
   function rentGame(uint trackingId) public payable returns(bool) {
